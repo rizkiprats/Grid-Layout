1a414d69e79b33624f756f71c15342ea83e4af7f
rizkiprats <riskiprats@gmail.com>
2021-12-03 19:03:25 +0700

    first commmit

---
+++ app/src/main/java/com/rizkiprats/programming/mygridlayout/model/AlphaChar.kt
@@ -0,0 +1,11 @@
+package com.rizkiprats.programming.mygridlayout.model
+
+class AlphaChar {
+    var iconsChar: Int ? = 0
+    var alphaChar: String? = null
+
+    constructor(iconsChar: Int?, alphaChar: String?) {
+        this.iconsChar = iconsChar
+        this.alphaChar = alphaChar
+    }
+}
